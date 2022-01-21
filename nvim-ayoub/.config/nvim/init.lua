cbf958a2ef5da02c497ed7da1a6d8e56010d6897
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-01-21 07:31:35 +0100

    update cmp

--- nvim-ayoub/.config/nvim/init.lua
+++ nvim-ayoub/.config/nvim/init.lua
@@ -1,12 +1,12 @@
 vim.g.mapleader = " "
 -- vim.cmd('set guifont="Fira Code:3"') -- ===
 vim.cmd('source /root/.config/nvim/vimScript/init.vim')
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained",
+-- require'nvim-treesitter.configs'.setup {
+--     ensure_installed = "maintained",
 
-    sync_install = false,
-    highlight = {enable = true, additional_vim_regex_highlighting = false}
-}
+--     sync_install = false,
+--     highlight = {enable = true, additional_vim_regex_highlighting = false}
+-- }
 require('ay')
 require('tsp')
 
--- nvim-ayoub/.config/nvim/lua/ay/conf/ay_cmp.lua
+++ nvim-ayoub/.config/nvim/lua/ay/conf/ay_cmp.lua
@@ -1,60 +1,81 @@
--- to make snippet change whe add any char
--- know i'm disable because TextChangedI make text write randomly on screen
--- require'luasnip'.config.setup({updateevents="TextChangedI" })
--- to load vs code snippet
--- must loaded after luasnip call snippet
-vim.cmd('set rtp+=/projects/lua/friendly-snippets')
-require('luasnip.loaders.from_vscode').load()
--- snippets/flutter.json
 
 vim.cmd([[
+        set rtp+=/projects/lua/friendly-snippets
         autocmd BufWritePost *.json lua require("luasnip").snippets={}
         autocmd BufWritePost *.json lua require("luasnip.loaders.from_vscode").load()
 ]])
---
--- Set completeopt to have a better completion experience
--- tis some visiter
---
--- vim.o.completeopt = "menu,menuone,noselect"
-vim.o.completeopt = 'menuone,noselect'
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then return end
 
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then return end
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+local check_backspace = function()
+    local col = vim.fn.col "." - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 end
--- luasnip setup
-local luasnip_status_ok, luasnip = pcall(require, 'luasnip')
-if not luasnip_status_ok then return end
+--   פּ ﯟ   some other good icons
+local kind_icons = {
+    Text = "",
+    Method = "m",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "",
+    Interface = "",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = ""
+}
+-- find more here: https://www.nerdfonts.com/cheat-sheet
 
--- nvim-cmp setup
-local smp_status_ok, cmp = pcall(require, 'cmp')
-if not smp_status_ok then return end
 cmp.setup {
     snippet = {
         expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
         end
     },
     mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm {behavior = cmp.ConfirmBehavior.Replace, select = true},
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), {"i", "c"}),
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), {"i", "c"}),
+        ["<C-Space>"] = cmp.mapping {i = cmp.mapping.abort(), c = cmp.mapping.close()},
+        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+        ["<C-e>"] = cmp.mapping {i = cmp.mapping.abort(), c = cmp.mapping.close()},
+        -- Accept currently selected item. If none selected, `select` first item.
+        -- Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.mapping.confirm {select = true},
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
+            elseif luasnip.expandable() then
+                luasnip.expand()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
+            elseif check_backspace() then
+                fallback()
             else
                 fallback()
             end
         end, {"i", "s"}),
-
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
@@ -65,44 +86,18 @@ cmp.setup {
             end
         end, {"i", "s"})
     },
-    sources = {
-        {name = 'path'}, {name = 'luasnip'}, {name = 'nvim_diagnostic'}, {name = 'cmp_tabnine'}, {name = 'treesitter'}, {name = 'buffer'}
-        -- { name = 'nvim_lua', keyword_lengh=1 },
-        -- { name = 'look' , keyword_lengh=0 },
-        -- { name = 'spell' },
-        -- { name = 'nuspell' },
-        -- { name = "user_dictionary" },
-        -- { name = "latex_symbols" },
-        -- { name = 'emoji' },
-
-    },
     formatting = {
-        -- fields = {"kind", "abbr", "menu"},
-        fields = {"kind", "abbr"},
+        fields = {"kind", "abbr", "menu"},
         format = function(entry, vim_item)
-            -- fancy icons and a name of kind
-            -- vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
-            vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " "
-            -- set a name for each source
-            vim_item.menu = ({
-                buffer = "[Buffer]",
-                calc = "[Calc]",
-                cmp_tabnine = "[TabNine]",
-                emoji = "[Emoji]",
-                latex_symbols = " [Latex_symbols]",
-                look = "[Look]",
-                luasnip = " [Luasnip]",
-                nuspell = " [Nuspell]",
-                nvim_diagnostic = "[LSP]",
-                nvim_lua = "[Lua]",
-                path = "[Path]",
-                spell = "[Spell]",
-                treesitter = " [Treesitter]",
-                user_dictionary = " [User_dictionary]"
-
-            })[entry.source.name]
+            -- Kind icons
+            vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            vim_item.menu = ({nvim_lsp = "[LSP]", luasnip = "[Snippet]", buffer = "[Buffer]", path = "[Path]"})[entry.source.name]
             return vim_item
         end
     },
-    experimenal = {ghost_text = true, native_menu = false}
+    sources = {{name = "nvim_lsp"}, {name = "luasnip"}, {name = "buffer"}, {name = "path"}},
+    confirm_opts = {behavior = cmp.ConfirmBehavior.Replace, select = false},
+    documentation = {border = {"╭", "─", "╮", "│", "╯", "─", "╰", "│"}},
+    experimental = {ghost_text = true, native_menu = false}
 }
