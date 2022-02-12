64df43497329c64cfdc778f5ad97e9947f27101b
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-02-13 00:07:07 +0100

    prompte change by distro

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
     if test -n "$SSH_TTY"
-        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+        echo -n (set_color brred)"$user:"''
     end
 
     echo -n (set_color blue)(prompt_pwd)' '
---
+++ void-fish/.config/fish/conf.d/user.fish
@@ -0,0 +1,5 @@
+#!/bin/fish
+
+set user 'youssef'
+
+
