b2a331f51fe6e35321228f201ec7fe229962a47d
root <root@759438755hax.localdomain>
2022-03-22 01:22:57 +0100

    add alias for void only alias.fish

---
+++ void-fish/.config/fish/conf.d/alias.fish
@@ -0,0 +1,7 @@
+
+alias ls='clear ; echo ;/bin/exa  --color=always --group-directories-first'
+alias nls='/bin/exa --color=always --group-directories-first'
+alias la='/bin/exa -a --color=always --group-directories-first'
+alias ll='/bin/exa -l --color=always --group-directories-first'
+
+
