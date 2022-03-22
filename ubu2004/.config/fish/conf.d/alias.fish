0733d8320a96a01173ad809a15042a77b1c91cfc
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-03-22 01:33:43 +0100

    init ubu

---
+++ ubu2004/.config/fish/conf.d/alias.fish
@@ -0,0 +1,6 @@
+alias cat='/bin/cat'
+
+alias ls='clear ; echo ;/bin/ls  --color=always --group-directories-first'
+alias nls='/bin/ls --color=always --group-directories-first'
+alias la='/bin/ls -a --color=always --group-directories-first'
+alias ll='/bin/ls -l --color=always --group-directories-first'
