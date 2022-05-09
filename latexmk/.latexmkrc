a8892ab60254624bebada260467d585f64157f8b
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-05-09 18:40:55 +0100

    up

--- latexmk/.latexmkrc
+++ latexmk/.latexmkrc
@@ -1,1 +1,5 @@
+# $pdflatex = 'pdflatex --shell-escape %O %S'
+# $pdf_previewer = "cp -f build/%A.pdf tmpview.pdf";
 $pdf_previewer = "zathura %O %S";
+# $success_cmd = 'cp -f build/%A.pdf tmpview.pdf';
+#$bibtex_fudge=1
--- void-fish/.config/fish/conf.d/alias.fish
+++ void-fish/.config/fish/conf.d/alias.fish
@@ -5,7 +5,7 @@ set packerdir "$HOME/.local/share/nvim/site/pack/packer/start"
 
 alias cat='/bin/bat -p'
 
-alias ls='clear ; echo ;/bin/exa --icons --color=always --group-directories-first'
+alias ls='clear ;/bin/exa --icons --color=always --group-directories-first'
 alias nls='/bin/exa --icons --color=always --group-directories-first'
 alias la='/bin/exa -a --icons --color=always --group-directories-first'
 alias ll='/bin/exa -l --icons --color=always --group-directories-first'
