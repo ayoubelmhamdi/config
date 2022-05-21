dbf8249c47cbecbdb1cc218221d4a9e114f53bb0
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-05-21 13:07:19 +0100

    up

--- fish/.config/fish/functions/git.fish
+++ fish/.config/fish/functions/git.fish
@@ -3,5 +3,5 @@
 function clone --description "clone something, cd into it. install it."
     git clone --depth=1 $argv[1]
     cd (basename $argv[1] | sed 's/.git$//')
-    yarn install
+    # yarn install
 end
