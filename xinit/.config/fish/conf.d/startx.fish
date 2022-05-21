fcea7c9a9a72cfe82deee958a9f04cfde753c9d0
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-05-21 13:23:50 +0100

    make date up must posiible by xinit only

--- xinit/.config/fish/conf.d/startx.fish
+++ xinit/.config/fish/conf.d/startx.fish
@@ -3,6 +3,9 @@
 #startx
 if status --is-login
   if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+     date --set='12 may 2021 05:05:05'
+     ayoub2 &!
+     vim --headless -c 'qall' &
      mkdir -p /tmp/ayoub
      echo "1 1" > /tmp/ayoub/cpu
      echo " " > /tmp/ayoub/status
