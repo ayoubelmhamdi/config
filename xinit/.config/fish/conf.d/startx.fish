6dcf3b906e05ab3f6f83d119c27f886fc295fa8d
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-05-25 16:39:08 +0100

    up

--- xinit/.config/fish/conf.d/startx.fish
+++ xinit/.config/fish/conf.d/startx.fish
@@ -4,7 +4,7 @@
 if status --is-login
   if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
      date --set='21 may 2022 05:05:05'
-     ayoub2 &!
+     ayoub2 &
      vim --headless -c 'qall' &
      mkdir -p /tmp/ayoub
      echo "1 1" > /tmp/ayoub/cpu
