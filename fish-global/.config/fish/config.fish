869b1fd6e548888ecf20704c4c831600f796abda
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-05-25 17:50:42 +0100

    fin export vars

--- fish-global/.config/fish/config.fish
+++ fish-global/.config/fish/config.fish
@@ -28,3 +28,18 @@ make_completion g 'git'
 
 set -gx EDITOR nvim
 #cd /projects/c/suckless/dwm
+
+function addpaths
+    contains -- $argv $fish_user_paths
+       or set -U fish_user_paths $fish_user_paths $argv
+    echo "Updated PATH: $PATH"
+end
+
+function removepath
+    if set -l index (contains -i $argv[1] $PATH)
+        set --erase --universal fish_user_paths[$index]
+        echo "Updated PATH: $PATH"
+    else
+        echo "$argv[1] not found in PATH: $PATH"
+    end
+end
--- fish-global/.config/fish/path.fish
+++ fish-global/.config/fish/path.fish
@@ -1,10 +1,10 @@
-
-
-# go bin folder
-set -x -g PATH ~/go/bin $PATH
-
-# rust bin folder
-set -x -g PATH ~/.cargo/bin $PATH
-
-# User bin folder
-set -x -g PATH ~/bin ~/.local/bin $PATH
+#
+#
+# # go bin folder
+# set -x -g PATH ~/go/bin $PATH
+#
+# # rust bin folder
+# set -x -g PATH ~/.cargo/bin $PATH
+#
+# # User bin folder
+# set -x -g PATH ~/bin ~/.local/bin $PATH
