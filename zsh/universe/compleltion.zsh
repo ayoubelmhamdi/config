8259cf03a0ac1c643cf1e45fe7e3241025c673a7
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-08-14 19:48:05 +0100

    init

---
+++ zsh/universe/compleltion.zsh
@@ -0,0 +1,11 @@
+
+
+# goup completion `$app<TAB>`
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*:*:-command-:*:*' group-order alias parameter builtins functions commands
+# zstyle ':completion:*' file-list all
+
+# complete `$ app <space> - <tab>` by intelegent ^_^
+zstyle ':completion:*' complete-options true
+
+
