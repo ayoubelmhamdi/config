4ce55bf524818d30e9d43912e694ceff281f2e70
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-09-04 03:11:44 +0100

    up

--- zsh/universe/compleltion.zsh
+++ zsh/universe/compleltion.zsh
@@ -5,7 +5,7 @@ zstyle ':completion:*' group-name ''
 zstyle ':completion:*:*:-command-:*:*' group-order alias parameter builtins functions commands
 # zstyle ':completion:*' file-list all
 
-# complete `$ app <space> - <tab>` by intelegent ^_^
+# complete `$ app<space>-<tab>` by intelegent ^_^
 zstyle ':completion:*' complete-options true
 
 
