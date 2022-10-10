0d6fad9afcfcc136d0236bc550dad648aa44cf24
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-10 02:59:57 +0100

    up

--- vnc/.vnc/closevnc.sh
+++ vnc/.vnc/closevnc.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # adb kill-server
 pkill x11vnc
-xrandr --output VIRTUAL1 --off
-xrandr --delmode VIRTUAL1 "800x1280_60.00"
-xrandr --rmmode "800x1280_60.00"
+# xrandr --output VIRTUAL1 --off
+# xrandr --delmode VIRTUAL1 "800x1280_60.00"
+# xrandr --rmmode "800x1280_60.00"
 
+xrandr --output VIRTUAL1 --off
+xrandr --delmode VIRTUAL1 "1280x800_60.00"
+xrandr --rmmode "1280x800_60.00"
