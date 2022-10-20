3bdf06ee3cab7bc4101547da40f8ea23cb7673ff
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-10-20 16:24:59 +0100

    init

--- zsh/universe/wine.zsh
+++ zsh/universe/wine.zsh
@@ -1,3 +1,3 @@
 
 alias foxit='wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Foxit\ Software/Foxit\ Reader/FoxitReader.exe'
-alias matlab='wine ~/.wine/drive_c/Program\ Files/MATLAB/R2010a/bin/matlab.exe'
+#alias matlab='wine ~/.wine/drive_c/Program\ Files/MATLAB/R2010a/bin/matlab.exe'
