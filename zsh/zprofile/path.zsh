5a11926d98270a3c6750e5a250aed0a8605c69d8
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-03-03 01:03:33 +0100

    (path.zsh) add cuda and c source/bin apps

--- zsh/zprofile/path.zsh
+++ zsh/zprofile/path.zsh
@@ -1,23 +1,29 @@
 MY_PATH=
 
+# USER c codes
+export MY_PATH=/data/projects/c/ayoub/i3status/bin:$MY_PATH
+
+# USER bash scripts
+export MY_PATH=$HOME/scripts/bin:$MY_PATH
+
+# User downloaded apps
+export MY_PATH=$HOME/bin:$HOME/.local/bin:$MY_PATH
+
 # go
 export MY_PATH=$HOME/go/bin:$MY_PATH
 
 # rust
 export MY_PATH=$HOME/.cargo/bin:$MY_PATH
 
-# User
-export MY_PATH=$HOME/bin:$HOME/.local/bin:$MY_PATH
-
 # flutter
 export MY_PATH=/app/flutter/bin:$MY_PATH
-
-# USER scripts
-export MY_PATH=$HOME/scripts/bin:$MY_PATH
-
 
 # Android-Studio
 export MY_PATH=/app/android-studio/bin:$MY_PATH
 
+# cuda
+export MY_PATH="/usr/local/cuda-10.1/bin:$MY_PATH"
+export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64:$LD_LIBRARY_PATH
 # FINAL
 export PATH=$MY_PATH:$PATH
+
