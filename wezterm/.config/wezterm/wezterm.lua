9cbe2a8963f7e4007db00b197ae7c1b5b52c4d52
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-04-17 00:33:43 +0000

    init

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font("Cascadia Code")
+-- config.color_scheme = 'GitHub Dark'
+config.color_scheme = "Gruvbox Material (Gogh)"
+
+return config
