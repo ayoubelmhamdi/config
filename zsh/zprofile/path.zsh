32f3bae37efa58bf9529a293975e517777257246
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-04-27 10:02:26 +0100

    (path.zsh) refactoring for full robostic codding in flutter and android

--- zsh/zprofile/path.zsh
+++ zsh/zprofile/path.zsh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-# ANDROID
-export ANDROID_SDK_HOME="/app/sdk/android-HOME"
-export ANDROID_SDK_ROOT="/app/sdk/android"
-export GRADLE_USER_HOME="/app/sdk/gradlec"
-# export ANDROID_HOME="/app/sdk/android"                        # Deprecated: Don't use it
-# export GRADLE_HOME="/app/sdk/gradle/gradle-7.3"               # flutter doesn't need it
-# export JAVA_HOME="$HOME/.cache/ltex-ls-15.2.0/jdk-11.0.12+7"  # package manager(xbps) openjdk 17.0.10
+#sdkmanager --update
+#yes | sdkmanager --licenses
+#sdkmanager --install "build-tools;34.0.0" "platforms;android-34" "platform-tools"
 
+export ANDROID_SDK_HOME="/data/sdk/androidHome"
+export ANDROID_SDK_ROOT="/data/sdk/android"
+export GRADLE_USER_HOME="/data/sdk/gradle"
+export GRADLE_HOME="/data/sdk/gradle/gradle-8.7"
+export PATH="$PATH:/data/sdk/flutter/bin:/data/sdk/android-studio/bin:/data/sdk/gradle/gradle-8.7/bin:/data/sdk/android/cmdline-tools/latest/bin:/data/sdk/android/platform-tools"
+# export ANDROID_HOME="/app/sdk/android"         # Deprecated: Don't use it
 
 
 # LIBRARY
-export ERG_PATH=/home/mhamdi/.erg
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.1/lib64
+# export ERG_PATH=/home/mhamdi/.erg
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.1/lib64
 # export PYTHONPATH=$PYTHONPATH:/data/github/lyrapdf
 
 declare -a MY_PATHS=(
@@ -23,23 +25,10 @@ declare -a MY_PATHS=(
     "$HOME/go/bin"
     "$HOME/.cargo/bin"
     "$HOME/.erg/bin"
-    "/app/flutter/bin"
-    "/app/android-studio/bin"
-    # "/app/sdk/gradle/gradle-7.3/bin"                          # flutter doesn't need it
     "/usr/local/cuda-10.1/bin"
-    "$ANDROID_SDK_ROOT/cmdline-tools/latest/bin"
-    "$ANDROID_SDK_ROOT/platform-tools"
 )
 
-# mkdir -p $ANDROID_SDK_ROOT
-# unzip commandlinetools-linux-11076708_latest.zip -d /tmp
-# mkdir -p $ANDROID_SDK_ROOT/cmdline-tools
-# mv /tmp/cmdline-tools $ANDROID_SDK_ROOT/cmdline-tools/latest
-# unzip platform-tools-latest-linux.zip -d $ANDROID_SDK_ROOT
-# sdkmanager "build-tools;34.0.0" "platforms;android-34" "platform-tools"
 
-
-# Iterate over the array and append each path to the PATH variable
 MY_PATH=""
 for _path in "${MY_PATHS[@]}"; do
   if [ -e "$_path" ];then
@@ -48,5 +37,3 @@ for _path in "${MY_PATHS[@]}"; do
 done
 
 export PATH=$PATH:$MY_PATH
-# export PATH="$PATH"
-# export PATH="$MY_PATH"
