f1595295f4ef931d306e82af0ba5435fe0615e64
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-09-05 05:16:09 +0100

    refactore zsh universe to be use many cli instead of zshrc configs

--- zsh/universe/cd.zsh
+++ zsh/universe/cd.zsh
@@ -21,18 +21,6 @@ fork(){
     fi
 }
 
-#function z
-#    if test -d $argv
-#        cd $argv
-#        zoxide add
-#    else
-#        set zres ( zoxide query $argv|cut -c8-)
-#        if test (count $zres) -gt 0
-#            cd $zres
-#        end
-#    end
-#end
-
 cd (){
     if [ $# -eq 0 ];then
         # builtin cd $HOME
@@ -79,5 +67,62 @@ cd (){
             "pack"*) builtin cd "$packerdir" ;;
             *) echo 'zoxide: no match found !!' ;;
         esac
+    fi
+}
+
+cdf(){
+    file=${1-}
+    if [[ ! -z $file ]];then
+        if [[ -f $file ]];then
+           cd $(dirname "$file")
+        else
+            echo -e "\n$file not exist\n"
+        fi
+    else
+        echo -e '\ncd to dir of this file\n'
+    fi
+}
+
+cf(){
+  if [ -z "$1" ]; then
+    echo "usage: cf <file>"
+    exit 1
+  fi
+  builtin cd $(dirname $(which $1))
+}
+
+
+cd_book(){
+    BOOK="/data/book"
+
+    case ${1:-} in
+        [tT]|[tT][rR][pP][mM])
+            DIR="TRPM"                    ;;
+        [aA]|[aA][yY][oO][uU][bB])
+            DIR="ayoubelmhamdi.github.io" ;;
+        [rR]|[rR][uU][sS][tT])
+            DIR="rust-lang.org"           ;;
+        [iI]|[iI][nN][fF][oO])
+            DIR="info"                    ;;
+        [pP]|[pP][rR][oO][jJ][eE][cC][tT][sS])
+            DIR="projects"                ;;
+        [lL]|[lL][iI][nn][uU][xX])
+            DIR="linux"                   ;;
+        [nN]|[nN][eE][wW])
+            DIR="new"                     ;;
+        *)  echo "no book at $BOOK";return;;
+    esac
+
+    builtin cd $BOOK/$DIR
+    # nvim -c 'lua require("harpoon.ui").toggle_quick_menu()'
+}
+
+
+cd_git_root(){
+    GITROOT="$(git rev-parse --show-toplevel)"
+    if [ -d "$GITROOT" ];then
+        builtin cd "$GITROOT"
+    else
+        echo "no git in this path"
     fi
 }
--- zsh/universe/zsh-alias.zsh
+++ zsh/universe/zsh-alias.zsh
@@ -9,56 +9,30 @@
 
 # Utilities
 
-# testin aliases
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
 
+
+alias ls='normalls'
+alias cal='cal -m'
+alias vc='dvim c'
+alias nnn='NNN_FIFO="${TMPFILE:-/tmp}/nnn.fifo" NVIM_APPNAME=nvim-ayoub nnn'
 alias py='python3'
 alias unstow='stow -D'
 alias ipy="python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
 
 alias upip='pip install -U pip'
 alias fg='fg 2>/dev/null|| true'
-alias termux='command termux'
-alias termuxsd='command termux -t sd'
-
-# TODO: mouve to cd
-source $HOME/scripts/app/cdf
+alias termuxsd='termux -t sd'
 
 alias stat="stat -c '%a  user:%U group:%G'"
-
 alias lsblk='echo;lsblk --output LABEL,FSUSE%,NAME,MOUNTPOINT,UUID'
-# alias ip=/usr/local/bin/ip
 alias cu='curl -LO'
 alias c=clear
 alias history='history 0|fzf'
-
-alias nv='devour neovide --nofork'
-
-
-function ig () {
- command grep -i --color=auto $@
-}
-
-
-# server http
-# alias live='setsid /root/.cargo/bin/live-server -h localhost -p 8001 >/dev/null 2>&1'
-
-# swallow by devour /sbin/mpv
-# alias mpv='devour /sbin/mpv'
 alias time='/usr/bin/time'
-
-
-
-
-#alias ls='clear ; echo ; ;exa  --color=always --group-directories-first'
-#alias nls='exa    --color=always --group-directories-first'
-#alias la='exa -a --color=always --group-directories-first'
-#alias ll='exa -l --color=always --group-directories-first'
-
-
 alias df="echo ;df -x squashfs -x tmpfs -x devtmpfs -h"
-# alias ddd='clear ;echo ; du -ah --max-depth 1|sort --sort=human-numeric --human-numeric-sort -r;'
-
-
 
 alias main="git checkout main ^ /dev/null || git checkout master"
 alias master="main"
@@ -70,120 +44,56 @@ alias push='git push '
 alias pull='git pull '
 alias sta='git status -s'
 alias up='git commit -m "up"'
-
-
-alias untar='tar xvf'
-alias vzip='zip -sf'
-# alias vtar='~/scripts/bin/vtar'
-# alias c='clear && tmux clear-history  >/dev/null 2>&1;echo'
-
-
-# function ls(){
-#   clear 
-#   /bin/exa --icons --color=always --group-directories-first "$@"
-# }
-# function nls(){
-#   /bin/exa --icons --color=always --group-directories-first "$@"
-# }
-# function la(){
-#   /bin/exa -a --icons --color=always --group-directories-first "$@"
-# }
-# function ll(){
-#   /bin/exa -l --icons --color=always --group-directories-first "$@"
-# }
-#
-
-
-
-
-
 alias gc='git clone --depth=1'
 alias co='git commit -m'
 
 alias log='git log --oneline'
 alias lg='git lg'
-# alias gdiff="git diff | sed -E 's#^(.{40}).*#\1...#' | PAGER='bat -p' delta"
-# alias show_full="git show |  PAGER='bat -p' delta"
-# alias gdiff="    git diff | PAGER='bat -p' delta"
 
-show(){
-  local file="${1:-}"
-  if [ -z "$file" ];then
-    git show | sed -E 's#^(.{40}).*#\1...#' | PAGER='bat -p' delta
-  else
-    git show $file | sed -E 's#^(.{40}).*#\1...#' | PAGER='bat -p' delta
-  fi
-}
 
-gdiff(){
-  local file="${1:-}"
-  if [ -z "$file" ];then
-    git diff | sed -E 's#^(.{140}).*#\1...#' | PAGER='bat -p' delta
-  else
-    git diff $file | sed -E 's#^(.{140}).*#\1...#' | PAGER='bat -p' delta
-  fi
-}
+
+
+alias untar='tar xvf'
+alias vzip='zip -sf'
+
 
 # xclip
 alias xc='xclip -selection clipboard'
+alias xo='xclip -o -sel clip'
+
+
 alias du='du -h '
 alias wget='wget -c'
 
 # nvim alias
-# alias v='nvim -u $avim/init.lua'
-alias pager="nvim -c 'setf markdown|set noswapfile| setlocal buftype=nofile'  -"
+alias pager="NVIM_APPNAME=nvim-ayoub nvim -c 'setf markdown|set noswapfile| setlocal buftype=nofile'  -"
 alias which='command -v'
-#alias e='nvimdir=~/.config/nvim nvim'
-# alias v='nvim'
-# alias vv='v -c "Telescope frecency default_text=:CWD:"'
-alias atmux='tmux a|| tmux'
-
-#alias ping='ping -c 1'
-alias df_device="df -h -kHl | grep -v 'cgroup\|tmp'"
-# # typos
-# abbr gti git
-# abbr yearn yarn
-# abbr py python
-# abbr -a -- - 'cd -'
-
-
-
-# alias cat='bat -p'
-#
-# alias ls='clear ;exa --icons --color=always --group-directories-first'
-# alias nls='exa   --icons --color=always --group-directories-first'
-# alias la='exa -a --icons --color=always --group-directories-first'
-# alias ll="exa -abghHlS --icons --git --group-directories-first"
 
 
 alias lxl='xbps-query -o' #local xlocate file /bin/usr/wget : output is wget-2-3
 alias xl='xlocate'
 
 
-alias sysinfo="inxi -Fxxxz"
+alias sysinfo="inxi -Fxxxz" # we need to check the inxi if exist
 
 alias liber=libreoffice
-setopt auto_pushd
-setopt pushd_ignore_dups
-setopt pushdminus
+alias lb=libreoffice
 
 
-alias cd..="builtin cd .." # Typo addressed.
+
 alias -- -="builtin cd -"
 alias cd_='builtin cd $_'
 alias ..='builtin cd ..'
 alias ...='builtin cd ../..'
 alias ....='builtin cd ../../..'
 
-alias cg=cd_git_root
-
 if command -v unbuffer >/dev/null;then
-  alias ujq="unbuffer jq"
+  alias jq="unbuffer jq"
+else
+  alias jq="jq"
 fi
 
-function jqv(){
-  command jq $@ | nvim +Man! +"setf json"
-}
+
 
 if command -v doas >/dev/null;then
   alias sudo=doas
@@ -192,3 +102,12 @@ fi
 if command -v openvpn >/dev/null;then
   alias openvpn='sudo openvpn --data-ciphers AES-256-GCM:AES-128-GCM:CHACHA20-POLY1305:AES-128-CBC --config'
 fi
+
+
+\=() {
+  math-eval "$@"
+}
+
+function jqv(){
+  command jq $@ | NVIM_APPNAME=nvim-ayoub nvim +Man! +"setf json foldlevel=99 nornu buftype=nofile noswapfile'
+}
