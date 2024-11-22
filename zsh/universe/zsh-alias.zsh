3dbb860210b58229a7a1f291ab60d46fc143dbed
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-11-22 21:43:06 +0100

    (zsh-alias.zsh) refacotr aliases

--- zsh/universe/zsh-alias.zsh
+++ zsh/universe/zsh-alias.zsh
@@ -15,6 +15,18 @@ setopt pushdminus
 
 
 # alias tmux='command tmux a 2>/dev/null || command tmux' # use one termux on every termials
+
+function check_command() {
+    command -v "$1" >/dev/null
+}
+
+
+alias cp="cp -ivr"
+alias mv='mv -iv'
+alias ncat='/sbin/cat'
+# alias cat='bat -p'
+
+
 alias pipu="pip install --upgrade pip"
 alias proj='builtin cd "$(shuff-projects)"'
 alias ls='normalls'
@@ -68,6 +80,7 @@ alias wget='wget -c'
 
 # nvim alias
 alias pager="NVIM_APPNAME=nvim-ayoub nvim -c 'setf markdown|set noswapfile| setlocal buftype=nofile'  -"
+alias PAGER="NVIM_APPNAME=nvim-ayoub nvim -c 'setf markdown|set noswapfile| setlocal buftype=nofile'  -"
 alias which='command -v'
 
 
@@ -75,7 +88,6 @@ alias lxl='xbps-query -o' #local xlocate file /bin/usr/wget : output is wget-2-3
 alias xl='xlocate'
 
 
-alias sysinfo="inxi -Fxxxz" # we need to check the inxi if exist
 
 alias liber=libreoffice
 alias lb=libreoffice
@@ -88,21 +100,12 @@ alias ..='builtin cd ..'
 alias ...='builtin cd ../..'
 alias ....='builtin cd ../../..'
 
-if command -v unbuffer >/dev/null;then
-  alias jq="unbuffer jq"
-else
-  alias jq="jq"
-fi
 
-
-
-if command -v doas >/dev/null;then
-  alias sudo=doas
-fi
-
-if command -v openvpn >/dev/null;then
-  alias openvpn='sudo openvpn --data-ciphers AES-256-GCM:AES-128-GCM:CHACHA20-POLY1305:AES-128-CBC --config'
-fi
+is_exist inxi && alias sysinfo="inxi -Fxxxz" # we need to check the inxi if exist
+is_exist unbuffer && alias jq="unbuffer jq"
+is_exist doas && alias sudo=doas
+is_exist openvpn && alias openvpn='sudo openvpn --data-ciphers AES-256-GCM:AES-128-GCM:CHACHA20-POLY1305:AES-128-CBC --config'
+[ -f /home/mhamdi/scripts/tools/cat ] && alias cat=/home/mhamdi/scripts/tools/cat
 
 
 \=() {
@@ -110,5 +113,5 @@ fi
 }
 
 function jqv(){
-  command jq $@ | NVIM_APPNAME=nvim-ayoub nvim +Man! +"setf json foldlevel=99 nornu buftype=nofile noswapfile"
+  jq "$@" | NVIM_APPNAME=nvim-ayoub nvim +Man! +"setf json foldlevel=99 nornu buftype=nofile noswapfile"
 }
