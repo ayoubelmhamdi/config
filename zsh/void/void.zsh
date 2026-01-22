6fc100ab0d1a930f67cda5320579922be0d316b0
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2026-01-22 10:02:05 +0100

    up

--- zsh/void/void.zsh
+++ zsh/void/void.zsh
@@ -5,3 +5,5 @@ export LINUX=void
 #local xlocate file /bin/usr/wget : output is wget-2-3
 alias lxl='xbps-query -o' 
 alias xl='xlocate'
+
+alias void-sync='sudo ntpdate 0.pool.ntp.org && sleep 1 && ping -c 1 repo-default.voidlinux.org && iu -S -y'
